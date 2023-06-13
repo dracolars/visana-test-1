eedb46ef9af2b08438491f9a17192defe0ef9e13
dracolars <dracolars@users.noreply.github.com>
2023-06-13 13:14:25 -0500

    testing get

---
+++ controllers/trades.js
@@ -0,0 +1,93 @@
+const Trades = require("../models/trades");
+
+// CRUD Methods
+
+// GET
+
+// GET /
+const getAllTrades = async (req, res, next) => {
+  try {
+    let type = req.query.type ? req.query.type : false;
+    let user_id = req.query.user_id ? req.query.user_id : false;
+    let trades = false;
+    console.log(type, user_id);
+    if (user_id && type) {
+      trades = await Trades.findAll({
+        where: {
+          user_id: user_id,
+          type: type,
+        },
+      });
+    } else if (type && !user_id) {
+      trades = await Trades.findAll({
+        where: {
+          type: type,
+        },
+      });
+    } else if (!type && user_id) {
+      trades = await Trades.findAll({
+        where: {
+          user_id: user_id,
+        },
+      });
+    } else {
+      trades = await Trades.findAll({});
+    }
+
+    if (trades) {
+      res.body = trades;
+      res.send(trades);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// GET /trades:id
+const getTradeById = async (req, res, next) => {
+  try {
+    let trade = await Trades.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (trade) {
+      res.body = trade;
+      res.send(trade);
+    } else {
+      res.status(404).send("ID not found");
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// POST
+const createTrade = async (req, res, next) => {
+  try {
+    const tradeObject = {
+      type: req.query.type,
+      user_id: Number(req.query.user_id),
+      symbol: req.query.symbol,
+      shares: Number(req.query.shares),
+      price: Number(req.query.price),
+      timestamp: Number(req.query.timestamp),
+    };
+    let newTrade = await Trades.create(tradeObject);
+    res.status(201).send(newTrade);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// DELETE, PUT, AND PATCH
+const handle405 = (req, res) => {
+  res.status(405).send();
+};
+
+module.exports = {
+  getAllTrades,
+  getTradeById,
+  createTrade,
+  handle405,
+};
--- routes/trades.js
+++ routes/trades.js
@@ -1,4 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const {
+  getAllTrades,
+  getTradeById,
+  createTrade,
+  handle405,
+} = require("../controllers/trades");
+
+router.get("/", getAllTrades);
+router.get("/:id", getTradeById);
+
+router.post("/", createTrade);
+
+router.delete("/:id", handle405);
+router.put("/:id", handle405);
+router.patch("/:id", handle405);
 
 module.exports = router;
